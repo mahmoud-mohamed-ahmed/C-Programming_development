3c52d5b988893c6acd4f54d949e012fbb1e881de
Mahmoud-Mohamed-Ahmed <mr.mahmoud.m.a1@gmail.com>
2020-05-22 14:43:22 +0200

    some Examples ofbult in math function

---
+++ cmath.cpp
@@ -0,0 +1,19 @@
+//Examlpes for bult in functon math
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int i=-5;
+	double x=5;
+	double y=2;
+	double z=5.21;
+	
+	cout<<"the absolut value of -5 ="<<abs(i)<<"\n";
+	cout<<"the power value of 5pwer2 ="<<pow(x,y)<<"\n";
+	cout<<"the floor value of 5.21="<<floor(z)<<"\n";	
+	return 0;
+}
+
+
+

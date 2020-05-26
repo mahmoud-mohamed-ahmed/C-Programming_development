f60fcacd893ed93727114aa1ac01c317a15bacef
Mahmoud-Mohamed-Ahmed <mr.mahmoud.m.a1@gmail.com>
2020-05-26 10:42:43 +0200

    multblucationTable'

---
+++ multblucationTable.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+		
+	for(int i=1; i<=10; i++)
+	{
+		cout<<"_________________________\n";
+		cout<<"\nMultibayfor Table ("<<i<<").\n";
+		for(int counter=1; counter<=10; counter++)
+		cout<<counter<<"* "<<i<<"= "<<counter*i<<endl;
+		return 0;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	return 0;
+}
